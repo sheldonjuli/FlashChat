e504233e91fbb583a6ab6f2ae9e344f67e17569b
sheldonjuli <sheldorosx@SheldorOSXs-Mac.local>
2018-07-21 07:12:21 -0700

    Removed backgound images on launch and welcome screen

--- Flash Chat/WelcomeViewController.swift
+++ Flash Chat/WelcomeViewController.swift
@@ -14,8 +14,7 @@ class WelcomeViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+            
     }
 
     override func didReceiveMemoryWarning() {
