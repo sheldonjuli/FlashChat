08810b2f17d3fe54c845c5c706476f14d24cb967
sheldonjuli <sheldorosx@SheldorOSXs-Mac.local>
2018-07-20 19:06:10 -0700

    Implimented login

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,7 +6,7 @@
 
 
 import UIKit
-
+import Firebase
 
 class LogInViewController: UIViewController {
 
@@ -29,6 +29,15 @@ class LogInViewController: UIViewController {
         
         //TODO: Log in the user
         
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            
+            if error != nil {
+                print("Log in error: \(error!)")
+            } else {
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
